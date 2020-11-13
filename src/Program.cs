29eb7832a57b0af5ec8ed5e0c20efe2bda65b48f
Sergey Andreev <sergey666andreev@gmail.com>
2020-11-13 11:40:47 +0600

    feat: Add empty project

---
+++ src/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace JobsFeedScraper
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
