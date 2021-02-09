149a403a4fe81e292a66f3851944274c0fc7d9cb
Sergey Andreev <sergey666andreev@gmail.com>
2021-02-09 10:25:00 +0600

    feat: Add helper for replacing special symbols

---
+++ src/JobsFeedScrapper.Service/ContentHelper.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JobsFeedScrapper.Service
+{
+    public static class ContentHelper
+    {
+        public static string ReplaceSpecialSymbols(string content)
+        { 
+            return content
+                .Replace("<br />", "\n")
+                .Replace("&nbsp;", " ")
+                .Replace("&bull;", "•")
+                .Replace("&auml;", "ä")
+                .Replace("&lsquo;", "'")
+                .Replace("&rsquo;", "'")
+                .Replace("&ntilde;", "ñ")
+                .Replace("&amp;", "&")
+                .Replace("&gt;", ">")
+                .Replace("&lt;", "<")
+                .Replace("&szlig;", "ß")
+                .Replace("&ndash;", "-");
+        }
+    }
+}
